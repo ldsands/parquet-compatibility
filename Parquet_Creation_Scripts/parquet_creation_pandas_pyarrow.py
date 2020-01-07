83be8415ce41933ec43d36409c1f65445a794a2c
Levi Sands <ldsands@outalook.com>
2020-01-07 08:05:42 -0600

    fixed the zstd issues

--- Parquet_Creation_Scripts/parquet_creation_pandas_pyarrow.py
+++ Parquet_Creation_Scripts/parquet_creation_pandas_pyarrow.py
@@ -5,25 +5,24 @@ import pathlib
 import pandas as pd
 
 p = pathlib.Path(__file__).parent.parent
-print(p)
+#print(p)
 
 Original_Data = p / "Original_Data"
 Parquet_Files = p / "Parquet_Files"
 
 compression_types = ["NONE", "SNAPPY", "GZIP", "ZSTD"]
 
-for compression_type in compression_types:
-    csv_files = Original_Data.glob("*.csv")
-    for file in csv_files:
+csv_files = Original_Data.glob("*.csv")
+for file in csv_files:
+    dta = pd.read_csv(file, delimiter="|")
+    for compression_type in compression_types:
         filename = (
             str(Parquet_Files.resolve())
             + "\\"
             + compression_type
             + "_pandas_pyarrow_"
-            + file.stem
+            + str(file.stem)
             + ".parquet"
         )
-        dta = pd.read_csv(file, delimiter="|")
-        schema = pa.Schema.from_pandas(dta)
-        dta = pa.Table.from_pandas(dta)
-        pq.ParquetWriter(filename, schema=schema, compression=compression_type)
+        print(filename)
+        dta.to_parquet(filename, compression=compression_type)
